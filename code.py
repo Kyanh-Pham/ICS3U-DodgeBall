dc5302fb44afca8f18c0e1d8e44b91d56b646e28
Kyanh-Pham <113041431+Kyanh-Pham@users.noreply.github.com>
2023-01-15 10:24:43 -0500

    Update code.py

--- code.py
+++ code.py
@@ -9,25 +9,24 @@ import ugame
 
 
 def game_scene():
-   # This function is the main game game_scene
+    # This function is the main game game_scene
 
-   image_bank_background = stage.Bank.from_bmp16("dodgeball_background.bmp")
-   image_bank_sprites = stage.Bank.from_bmp16("dodgeball_sprite.bmp")
+    image_bank_background = stage.Bank.from_bmp16("dodgeball_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("dodgeball_sprite.bmp")
 
-   background = stage.Grid(image_bank_background, 10, 8)
+    background = stage.Grid(image_bank_background, 10, 8)
 
-   ship = stage.Sprite(image_bank_sprites, 5, 75 ,66)
+    ship = stage.Sprite(image_bank_sprites, 5, 75 ,66)
 
-   game = stage.Stage(ugame.display, 60)
-   game.layers = [ship] + [background]
-   game.render_block()
+    game = stage.Stage(ugame.display, 60)
+    game.layers = [ship] + [background]
+    game.render_block()
 
-   # repeat forever, game loop
-   while True:
-       game.render_sprites([ship])
-       game.tick()
+    # repeat forever, game loop
+    while True:
+        game.render_sprites([ship])
+        game.tick()
 
 
 if __name__ == "__main__":
    game_scene()
-
